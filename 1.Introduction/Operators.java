afb3c264b2ec80456db0854016a4a1043a6c07dd
Ansh Nilesh Patel <patelansh883@gmail.com>
2021-07-16 23:55:47 +0530

    update

---
+++ 1.Introduction/Operators.java
@@ -0,0 +1,21 @@
+public class Operators {
+    public static void main(String[] Args){
+        System.out.println(9/5);
+        System.out.println(5/9);
+        System.out.println(9.0/5);
+        System.out.println(5.0/9);
+        System.out.println(9 + 5);
+        System.out.println(9 - 5);
+        System.out.println(9*5);
+        System.out.println(9 % 5);
+        System.out.println('A' + 1);
+        System.out.println('a' + 1);
+        System.out.println('a' + 'b');
+        char c = 'a';
+        int i = c + 3;
+        System.out.println(i);
+        char c2 = 'a';
+        char x = (char) (c + 3);
+        System.out.println(x);
+    }
+}
---
+++ 1.Introduction/Relational_Operators.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Relational_Operators {
+    public static void main(String[] Args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter i: ");
+        int i = scan.nextInt();
+        System.out.print("Enter j: ");
+        int j = scan.nextInt();
+        boolean k = i < j;
+        System.out.print("IS i is less then j: ");
+        System.out.print(k);
+
+    }
+
+
+}
