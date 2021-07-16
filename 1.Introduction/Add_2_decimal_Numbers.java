b6ddad8931ca144652d47bf082b43f9da1e48381
Ansh Nilesh Patel <patelansh883@gmail.com>
2021-07-16 19:37:38 +0530

    update

--- 1.Introduction/Add_2_decimal_Numbers.java
+++ 1.Introduction/Add_2_decimal_Numbers.java
@@ -1,9 +1,9 @@
 public class Add_2_decimal_Numbers {
 
     public static void main(String[] Args){
-        double i =  12.4;
-        double j = 1.45;
-        double k = i + j;
+        float i =  12.4f;
+        float j = 1.45f;
+        float k = i + j;
         System.out.print(k);
     }
 }
