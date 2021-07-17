954eabed2721a1aee7635d5d31a57a56babcfba9
Ansh Nilesh Patel <patelansh883@gmail.com>
2021-07-18 02:43:12 +0530

    update

---
+++ 3.Pattens/Patten_4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Patten_4 {
+    public static void main(String[] Args){
+        Scanner n = new Scanner(System.in);
+        int i = n.nextInt();
+        int j = 1;
+        while (j <= i){
+            int k = 1;
+            while (k <= i){
+                System.out.print("*");
+                k = k + 1;
+            }
+            System.out.println();
+            j = j + 1;
+        }
+    }
+}
